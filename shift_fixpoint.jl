99ba2c9edaa08fdce8ae13cd2b85c29bb731fb68
HASAYAKE <hasayake@pal-nat184-032-172.itap.purdue.edu>
2015-08-28 13:06:31 -0400

    initial commit

---
+++ shift_fixpoint.jl
@@ -0,0 +1,29 @@
+function shift_fix(P, v, alpha, gama, n, m)
+  maxiter = 10000
+  tol = 1/10^(8)
+  x_old = rand(n)
+  x_old = x_old/sum(x_old)
+  println("sfdadf")
+  for i = 1:maxiter
+    xm = x_old
+    for j = 1:m-1
+      xm = kron(xm, x_old)
+    end
+    x_new = (alpha/(1+gama))*P*xm + ((1-alpha)/(1+gama))*v + (gama/(1+gama))*x_old
+    res = sum(abs(x_new-x_old))
+    if res <= tol
+      println("find the fix point within ",i," iteration")
+      println("x = ", x_new)
+      break
+    end
+    println("---iter ",i," residual is ",res)
+    x_old = x_new
+  end
+end
+
+n=3;m=2
+P = transpose([1/3 1/3 1/3; 1/3 1/3 1/3; 1/3 1/3 1/3; 1/3 1/3 1/3; 0 0 1; 0 1/2 1/2; 0 1 0;0 0 1; 0 1 0])
+v = ones(3,1)/3
+alpha = 0.9
+gama = 0.8
+shift_fix(P,v,alpha,gama,n,m)
