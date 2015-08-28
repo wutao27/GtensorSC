4740cd3865c3678fe5e31d1b23d31daeaf0e757d
Tao Wu <wutao27@gmail.com>
2015-08-28 17:36:15 -0400

    sparse tensor implmentation

--- shift_fixpoint.jl
+++ shift_fixpoint.jl
@@ -1,15 +1,52 @@
+# --------------- These codes compute the multilinear pagerank problem ---------------------
+#compute the kron of x
+function compute_kron(index, x, m)
+  index = index - 1
+  ind_vec = zeros(m)
+  count = 1
+  while(index>=n)
+    ind_vec[count] = index%n
+    count+=1
+    index = index/n
+  end
+  result = 1
+  for i in 1:m
+    result = result*x[ind_vec[i]+1]
+  end
+  return result
+end
+
+# row_vec, col_vec, val_vec is a sparse n^m by n matrix, x is a n-dim vector
+function sparse_kron(row_vec, col_vec, val_vec, x, m)
+  n = size(x)[1];Px = zeros(n)
+  col = col_vec[1];sumval = 0
+  #println("debug")
+  for ind = 1:size(val_vec)[1]
+    if col_vec[ind]!= col
+      Px[col]=sumval
+      sumval = val_vec[ind]*compute_kron(col_vec[ind],x,m)
+      col = col_vec[ind]
+    else
+      sumval += val_vec[ind]*compute_kron(col_vec[ind],x,m)
+    end
+    if ind==size(val_vec)[1]
+      Px[col]=sumval
+    end
+  end
+  return Px
+end
+
+
+# P is a sparse n^m by n matrix
 function shift_fix(P, v, alpha, gama, n, m)
   maxiter = 10000
   tol = 1/10^(8)
   x_old = rand(n)
   x_old = x_old/sum(x_old)
-  println("sfdadf")
+  row_vec, col_vec, val_vec = findnz(P)
   for i = 1:maxiter
-    xm = x_old
-    for j = 1:m-1
-      xm = kron(xm, x_old)
-    end
-    x_new = (alpha/(1+gama))*P*xm + ((1-alpha)/(1+gama))*v + (gama/(1+gama))*x_old
+    Px = sparse_kron(row_vec, col_vec, val_vec, x_old, m)
+    x_new = (alpha/(1+gama))*Px + ((1-alpha)/(1+gama))*v + (gama/(1+gama))*x_old
     res = sum(abs(x_new-x_old))
     if res <= tol
       println("find the fix point within ",i," iteration")
@@ -21,10 +58,10 @@ function shift_fix(P, v, alpha, gama, n, m)
   end
 end
 
+# test case
 n=3;m=2
-P = transpose([1/3 1/3 1/3; 1/3 1/3 1/3; 1/3 1/3 1/3; 1/3 1/3 1/3; 0 0 1; 0 1/2 1/2; 0 1 0;0 0 1; 0 1 0])
+P = sparse([1/3 1/3 1/3; 1/3 1/3 1/3; 1/3 1/3 1/3; 1/3 1/3 1/3; 0 0 1; 0 1/2 1/2; 0 1 0;0 0 1; 0 1 0])
 v = ones(3,1)/3
 alpha = 0.9
 gama = 0.8
 shift_fix(P,v,alpha,gama,n,m)
-# test git second commit
